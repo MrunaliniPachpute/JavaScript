ee83e35b573588b31c3886716868434f34124cd4
MrunaliniPachpute <pachputemrunalini@gmail.com>
2024-11-25 19:09:45 +0530

    chapter 3

---
+++ Chapters3/scripts/index.js
@@ -0,0 +1,5 @@
+/*console.log('I am coming from index.js')
+//document.getElementById('click-me').click();
+//document.querySelector('#click-me').click();
+console.clear()*/
+alert('Welcome to my webpage buddy');
